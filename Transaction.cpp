27c7734127502ede695d22e485553e5009f00bb2
yuval <yuvalimac212@gmail.com>
2025-05-08 15:46:59 +0300

    transaction.cpp really implemented

--- Transaction.cpp
+++ Transaction.cpp
@@ -1,3 +1,19 @@
-//
-// Created by yuval on 5/8/2025.
-//
+#include <fstream>
+#include "Transaction.h"
+#include "Utilities.h"
+
+
+void TransactionDumpInfo(const Transaction& transaction, ofstream& file){
+    file <<"Sender Name: " << transaction.receiver <<"\n"
+    <<"Reciever Name: " << transaction.sender <<"\n"
+    <<"Transaction Value: " << transaction.value << std::endl;
+}
+
+string TransactionHashedMessage(const Transaction& transaction) {
+    return hash(transaction.value,transaction.sender,transaction.receiver);
+}
+
+bool TransactionVerifyHashedMessage(const Transaction& transaction,string hashedMessage) {
+    string hash = TransactionHashedMessage(transaction);
+    return hash == hashedMessage;
+}
