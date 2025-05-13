b9215ef29c07be1314ed4f6748cf2b8e7150b1ce
byk <byk@LAPTOP-M6JIHVAM.>
2025-05-13 15:19:26 +0300

    Change implementation some functions + add dump function

--- BlockChain.cpp
+++ BlockChain.cpp
@@ -1,31 +1,35 @@
 #include "BlockChain.h"
 #include <fstream>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
-//I don't know yet if it's supposed to be recursive...
+//Changed Implementation of first two functions, recursion is not needed
+
 
 int BlockChainGetSize(const BlockChain& blockChain) {
-    if (blockChain.previousBlock == nullptr) {
-        return 1;
+    int size = 0;
+    const BlockChain* currentBlock = &blockChain;
+    while (currentBlock != nullptr) {
+        size++;
+        currentBlock = currentBlock->previousBlock;
     }
-    return 1 + BlockChainGetSize(*blockChain.previousBlock);
+    return size;
 }
 
 int BlockChainPersonalBalance(const BlockChain& blockChain, const string& name) {
-    if (blockChain.previousBlock == nullptr) {
-        return 0;
-    }
     int balance = 0;
-    if (blockChain.transaction.receiver == name) {
-        balance += blockChain.transaction.value;
+    const BlockChain* currentBlock = &blockChain;
+    while (currentBlock != nullptr) {
+        if (currentBlock->transaction.receiver == name) {
+            balance += currentBlock->transaction.value;
+        }
+        else if (currentBlock->transaction.sender == name) {
+            balance -= currentBlock->transaction.value;
+        }
+        currentBlock = currentBlock->previousBlock;
     }
-    else if (blockChain.transaction.sender == name) {
-        balance -= blockChain.transaction.value;
-    }
-    return balance + BlockChainPersonalBalance(*blockChain.previousBlock, name);
+    return balance;
 }
 
 void BlockChainAppendTransaction(
@@ -54,25 +58,51 @@ void BlockChainAppendTransaction(
     newBlock->previousBlock = &blockChain;
 }
 
+//Worst function of mine, check it out please, pay attention that function returns by value
+
 BlockChain BlockChainLoad(ifstream& file) {
     string line;
-    getline(file, line);
-    istringstream firstLine(line);
     string sender, receiver, timestamp;
     unsigned int value;
-    firstLine >> sender >> receiver >> value >> timestamp;
-    BlockChain* head = new BlockChain;
-    head->transaction.sender = sender;
-    head->transaction.receiver = receiver;
-    head->transaction.value = value;
-    head->timeStamp = timestamp;
-    head->previousBlock = nullptr;
-    while (getline(file, line)) {
-        istringstream lineStream(line);
-        lineStream >> sender >> receiver >> value >> timestamp;
-        BlockChainAppendTransaction(*head, value, sender, receiver, timestamp);
+    BlockChain tail;
+    BlockChain* head = nullptr;
+    while (file >> sender >> receiver >> value >> timestamp) {
+        if (head == nullptr) {
+            tail.transaction.sender = sender;
+            tail.transaction.receiver = receiver;
+            tail.transaction.value = value;
+            tail.timeStamp = timestamp;
+            tail.previousBlock = nullptr;
+            head = &tail;
+        }
+        else {
+            BlockChain* newBlock = new BlockChain;
+            newBlock->transaction.sender = sender;
+            newBlock->transaction.receiver = receiver;
+            newBlock->transaction.value = value;
+            newBlock->timeStamp = timestamp;
+            newBlock->previousBlock = head;
+            head = newBlock;
+        }
     }
-    return *head;
+    BlockChain result = *head;
+    while (head != nullptr) {
+        BlockChain* ptr = head;
+        head = head->previousBlock;
+        delete ptr;
+    }
+    return result;
+}
+
+void BlockChainDump(const BlockChain& blockChain, ofstream& file) {
+    const BlockChain* currentBlock = &blockChain;
+    while (currentBlock->previousBlock != nullptr) {
+        file << "Sender name: " << currentBlock->transaction.sender << "\n"
+             << "Receiver name: " << currentBlock->transaction.receiver << "\n"
+             << "Transaction nalue: " << currentBlock->transaction.value << "\n"
+             << "Transaction timestamp: " << currentBlock->timeStamp << "\n";
+        currentBlock = currentBlock->previousBlock;
+    }
 }
 
 
