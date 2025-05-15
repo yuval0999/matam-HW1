cdb4804d25360d82ab412fd45fb3c557fb2c72b8
yuval <yuvalimac212@gmail.com>
2025-05-15 17:42:35 +0300

    implement format and compress

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include "BlockChain.h"
 #include <iostream>
 
+
 int main (int argc, char** argv) {
-    
+
+}
+
+
+
+
+
+
+
+
+
+void format(const BlockChain& block_chain , ofstream& target) {
+    BlockChainDump(block_chain, target);
+}
+
+void compress(BlockChain& block_chain, ofstream& target) {
+    BlockChainCompress(block_chain);
+    BlockChainDump(block_chain, target);
 }
